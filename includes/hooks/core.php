a22433d93b89d84e39d67a35bef504b29d8c88f3
Etherion99 <juanstt99@gmail.com>
2023-05-23 14:11:33 -0500

    add some hooks

--- includes/hooks/core.php
+++ includes/hooks/core.php
@@ -1,5 +1,26 @@
 <?php
 
-if ( ! defined('ABSPATH') ) {
+if ( ! defined('ABSPATH') )
     die('Direct access not permitted.');
+
+// Functions for hooks
+
+// Register wp schedule event for link analysis cron job
+function register_link_analysis_cron(): void {
+	if (!wp_next_scheduled('link_analysis_cron')) {
+		wp_schedule_event(time(), 'link_analysis_interval', 'link_analysis_cron');
+	}
 }
+
+// Create custom cron intervals
+function custom_cron_schedules( $schedules ) {
+	$schedules['link_analysis_interval'] = array(
+		'interval' => 5,
+		'display' => __('Link Analysis Interval')
+	);
+	return $schedules;
+}
+
+// Hooks configuration
+add_filter( 'cron_schedules', 'custom_cron_schedules');
+add_action( 'link_analysis_cron', 'find_broken_links' );
